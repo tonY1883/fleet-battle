0de911e4f1cf4e8087dc093aa7e3cd27cbdd75a6
wyrd <vol1883@gmail.com>
2017-12-26 16:38:41 +0800

    Add German ship tiles

--- script/game-utils.js
+++ script/game-utils.js
@@ -19,7 +19,7 @@ function getShipClass(Grid) {
 }
 
 function getShipTileImagesURL(shipsSet, classActing, status) {
-	if (img_url.ship_tiles_nation[shipsSet] !== undefined && img_url.ship_tiles_nation[shipsSet][classActing][status].length > 0) {
+	if (img_url.ship_tiles_nation[shipsSet] !== undefined && img_url.ship_tiles_nation[shipsSet].length > 0 && img_url.ship_tiles_nation[shipsSet][classActing][status].length > 0) {
 		return img_url.ship_tiles_nation[shipsSet][classActing][status];
 	} else {
 		return img_url.ship_tiles_default[classActing][status];
